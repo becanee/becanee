2a5800c56277c62821b833ad371cd47248d86b89
elcodee <ramdit21@gmail.com>
2024-08-08 00:54:32 +0700

    ✨ feat(Becanee): Not Found
    - Add page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import NotFoundComponent from '@/components/NotFoundComponent'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <>
+      <NotFoundComponent />
+    </>
+  )
+}
