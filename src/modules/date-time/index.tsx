136788a8b146aa62fe74a59b4a984377db943309
elcodee <ramdit21@gmail.com>
2025-08-11 09:04:00 +0700

    update toast

--- src/modules/date-time/index.tsx
+++ src/modules/date-time/index.tsx
@@ -14,13 +14,13 @@ export default function DateTimeNow({ intervalMs = 60_000 }: { intervalMs?: numb
         dismissible: true,
         closeButton: true,
         duration: Infinity,
-        position: 'top-right',
+        position: 'bottom-center',
         action: {
           label: 'Check it out',
           onClick: () => window.open('https://n8n.becaneee.xyz', '_blank'),
         },
       })
-    }, 6000);
+    }, 8000);
   }, [])
   return <span className="text-xl font-bold tracking-tighter sm:text-xl xl:text-xl/none" >{now}</span>;
 }
